dfa9ec1edc759ed813b0499721fefeee8228d7cd
Johnny <jriverabarzola@gmail.com>
2016-01-24 18:22:57 -0500

    Primer commit de prueba jrivera

---
+++ WcfService/Dominio/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService.Dominio
+{
+    public class Alumno
+    {
+        public int cd_alumno { get; set; }
+        public int cd_padre { get; set; }
+        public string ds_nombre { get; set; }
+        public string ds_apellido { get; set; }
+        public int cd_grado { get; set; }
+    }
+}
---
+++ WcfService/Dominio/Curso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService.Dominio
+{
+    public class Curso
+    {
+        public int cd_curso { get; set; }
+        public string ds_curso { get; set; }
+    }
+}
